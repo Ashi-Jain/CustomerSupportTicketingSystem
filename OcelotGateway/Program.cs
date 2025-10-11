07a31d22f0bec9d57af00dfa6bc03e4e287f59d3
Ashi Jain <ashij07@gmail.com>
2025-10-11 23:48:21 +0530

    Added CORS Policy

--- OcelotGateway/Program.cs
+++ OcelotGateway/Program.cs
@@ -1,10 +1,31 @@
-using Ocelot.DependencyInjection;
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var allowedOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowedOrigins, 
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseCors(allowedOrigins);
+
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
